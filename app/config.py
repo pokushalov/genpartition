fba56d17467a6865b53d7d0a0ccf90b72f3054ef
Alex Pokushalov <pokushalov@gmail.com>
2020-04-27 15:02:55 -0400

    Bugfix/Refactoring

---
+++ app/config.py
@@ -0,0 +1,10 @@
+database_connection = {
+    "connection_type": "direct",
+    "host_name": "localhost",
+    "service_name": "XE",
+    "port": "1521"
+}
+database_auth = {
+    "username": "test",
+    "password": "test"
+}
